4eac7bc2241fbd7cccc9b8bc17fd68fadb6c7ae6
PrijGalina <g_prij@mail.ru>
2021-05-31 21:29:41 +0300

    first function edit 1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 function getRandomInteger(min, max){
-  min = (min < 0) ? 0 : min;
-  max = (max > 0) ? max : 0;
+  min = min < 0 ? 0 : Math.floor(min);
+  max = max > 0 ? Math.floor(max) : 0;
   if(min < max && max !== 0) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;// источник - https://learn.javascript.ru/number
+    return Math.floor(Math.random() * (max - min + 1) + min);// источник - https://learn.javascript.ru/number
   }
-  return 'error';
+  throw new Error('getRandomInteger — максимальное число не может равняться нулю и должно быть больше минимального, значения float округляются до ближайшего целого');
 }
 
 getRandomInteger(5, 50);
 
 function getRandomFloat(min, max, digitsNum){
-  min = (min < 0) ? 0 : min;
-  max = (max > 0) ? max : 0;
+  min = min < 0 ? 0 : min;
+  max = max > 0 ? max : 0;
   if (min < max && max !== 0) {
-    const randomValue = Math.random() * (max - min + 1) + min;
+    const randomValue = Math.random() * (max - min) + min;
     return parseFloat(randomValue.toFixed(digitsNum));
   }
-  return 'error';
+  throw new Error('getRandomFloat — максимальное число не может равняться нулю и должно быть больше минимального');
 }
 
 getRandomFloat(0.1, 0.5, 3);
