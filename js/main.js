9d4f3910bef4df50377bb446e17ccc41727816e9
PrijGalina <g_prij@mail.ru>
2021-06-07 14:40:36 +0300

    module4-task1 edit 4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+/* Функция взята из интернета и доработана, Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random */
 
-const featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photoArray = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
+const LODGINGS = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
 const ADS_COUNT = 10;
 
@@ -23,9 +18,10 @@ const getRandomPositiveFloat = function (numValue, otherNumValue, digits = 1) {
   return result.toFixed(digits);
 };
 
-const getAuthor = () => {
+const getAuthor = function () {
+  const authorPictureLink = `img/avatars/user0${getRandomPositiveInteger(1, 8)}.png`;
   return {
-    avatar: 'img/avatars/user0' + getRandomPositiveInteger(1, 8) + '.png',
+    avatar: authorPictureLink,
   };
 };
 
@@ -37,58 +33,59 @@ const getRandomTime = function () {
 };
 
 const getRandomLodgingType = function () {
-  const lodgingArray = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  const lodgingArrayLength = lodgingArray.length - 1;
+  const lodgingArrayLength = LODGINGS.length - 1;
   const randomIndex = getRandomPositiveInteger(0, lodgingArrayLength);
-  return lodgingArray[randomIndex];
+  return LODGINGS[randomIndex];
 };
 
 const getRandomArray = function (customArray) {
-  const randomLengthArray = getRandomPositiveInteger(1 , customArray.length);
+  const randomLengthArray = getRandomPositiveInteger(1, customArray.length);
   const newArray = [];
-  for (let i = 0; i <= randomLengthArray-1; i++) {
+  for (let index = 0; index <= randomLengthArray-1; index++) {
     const newLengthCustomArray = customArray.length;
     const randomIndex = getRandomPositiveInteger(0, newLengthCustomArray - 1);
-    newArray[i] = customArray[randomIndex];
+    newArray[index] = customArray[randomIndex];
     customArray.splice(randomIndex, 1);
   }
   return newArray;
 };
 
-const getLocation = () => {
+const getLocation = function () {
   return {
     lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
     lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
   };
 };
 
-const getOffer = (location) => {
+const getOffer = function (features, photos, location) {
   return {
     title: 'Dacha. Дизайнерский домик во Всеволожске',
-    address: location.lat + ', ' + location.lng,
+    address: `${location.lat  }, ${  location.lng}`,
     price: getRandomPositiveInteger(1000, 50000),
     type: getRandomLodgingType(),
     rooms: getRandomPositiveInteger(1, 5),
     guests: getRandomPositiveInteger(1, 12),
     checkin: getRandomTime(),
     checkout: getRandomTime(),
-    features: getRandomArray(featuresArray),
+    features: getRandomArray(features),
     description: 'Дизайнерский домик с ламповой атмосферой. Только для хороших людей. Построен в 2019 г из двух сорокафутовых морских контейнеров',
-    photos: getRandomArray(photoArray),
+    photos: getRandomArray(photos),
   };
 };
 
 const createAds = () => {
   const location = getLocation();
+  const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  const PHOTOS = [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+  ];
   return {
     author: getAuthor(),
-    offer: getOffer(location),
+    offer: getOffer(FEATURES, PHOTOS, location),
     location: location,
   };
 };
 
 const adSet = new Array(ADS_COUNT).fill(null).map(() => createAds());
-
-adSet();
-
-//console.log(adSet);
+adSet;
