790f4ab3a505bce59de04bc7cba9b6ae9424f1bf
PrijGalina <g_prij@mail.ru>
2021-06-07 13:30:10 +0300

    singlequote

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-const featuresArray = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+const featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const photoArray = [
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg", "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg"
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 const ADS_COUNT = 10;
 
@@ -24,19 +24,19 @@ function getRandomPositiveFloat(numValue, otherNumValue, digits = 1) {
 
 const getAuthor = () => {
   return {
-    avatar: "img/avatars/user0" + getRandomPositiveInteger(1, 8) + ".png",
+    avatar: 'img/avatars/user0' + getRandomPositiveInteger(1, 8) + '.png',
   };
 };
 
 const getRandomTime = function () {
-  const timeArray = ["12:00","13:00","14:00"];
+  const timeArray = ['12:00','13:00','14:00'];
   const timeArrayLength = timeArray.length - 1;
   const randomIndex = getRandomPositiveInteger(0, timeArrayLength);
   return timeArray[randomIndex];
 }
 
 const getRandomLodgingType = function () {
-  const lodgingArray = ["palace", "flat", "house", "bungalow", "hotel"];
+  const lodgingArray = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
   const lodgingArrayLength = lodgingArray.length - 1;
   const randomIndex = getRandomPositiveInteger(0, lodgingArrayLength);
   return lodgingArray[randomIndex];
@@ -63,7 +63,7 @@ const getLocation = () => {
 
 const getOffer = (location) => {
   return {
-    title: "Dacha. Дизайнерский домик во Всеволожске",
+    title: 'Dacha. Дизайнерский домик во Всеволожске',
     address: location.lat + ', ' + location.lng,
     price: getRandomPositiveInteger(1000, 50000),
     type: getRandomLodgingType(),
@@ -72,7 +72,7 @@ const getOffer = (location) => {
     checkin: getRandomTime(),
     checkout: getRandomTime(),
     features: getRandomArray(featuresArray),
-    description: "Дизайнерский домик с ламповой атмосферой. Только для хороших людей. Построен в 2019 г из двух сорокафутовых морских контейнеров",
+    description: 'Дизайнерский домик с ламповой атмосферой. Только для хороших людей. Построен в 2019 г из двух сорокафутовых морских контейнеров',
     photos: getRandomArray(photoArray),
   }
 }
