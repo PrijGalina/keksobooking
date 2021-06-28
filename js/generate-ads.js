5a55f5638ae59f9312b82b1f541f32f12c66966c
PrijGalina <g_prij@mail.ru>
2021-06-29 00:28:23 +0300

    map

--- js/generate-ads.js
+++ js/generate-ads.js
@@ -1,50 +1,48 @@
 import {HOUSING_TYPE} from './data.js';
 import {getDeclension} from './util.js';
 
-const generateAds = (ads) => {
+const generateAd = (ad) => {
   const adsFragment = document.createDocumentFragment();
   const adsTemplate = document.querySelector('#card').content.querySelector('.popup');
-  ads.forEach((ad) => {
-    const adElement = adsTemplate.cloneNode(true);
-    ad.author.avatar ? adElement.querySelector('.popup__avatar').src = ad.author.avatar : adElement.querySelector('.popup__avatar').classList.add('hidden');
-    adElement.querySelector('.popup__title').textContent = ad.offer.title || '';
-    adElement.querySelector('.popup__text--address').textContent = ad.offer.address || '';
-    adElement.querySelector('.popup__text--price').innerHTML = ad.offer.price ? `${ad.offer.price}<span>₽/ночь</span>` : '';
-    adElement.querySelector('.popup__type').textContent = HOUSING_TYPE[ad.offer.type].name || '';
-    adElement.querySelector('.popup__text--capacity').textContent = (ad.offer.guests && ad.offer.rooms) ? `${ad.offer.rooms} ${getDeclension(ad.offer.rooms, ['комната', 'комнаты', 'комнат'])} для ${ad.offer.guests} ${getDeclension(ad.offer.guests, ['гостя', 'гостей', 'гостей'])}` : '';
-    adElement.querySelector('.popup__text--time').textContent = (ad.offer.checkin && ad.offer.checkout) ? `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}` : '';
-    adElement.querySelector('.popup__description').textContent = ad.offer.description || '';
-    if (ad.offer.features) {
-      const modifiers = ad.offer.features.map((feature) => `popup__feature--${feature}`);
-      const adFeaturesElements = adElement.querySelectorAll('.popup__feature');
-      adFeaturesElements.forEach((item) => {
-        const modifier = item.classList[1];
-        if (!modifiers.includes(modifier)) {
-          item.classList.add('hidden');
-        }
-      });
-    }
-    else {
-      adElement.querySelector('.popup__features').classList.add('hidden');
-    }
-    if (ad.offer.photos) {
-      const photoTemplate = adElement.querySelector('.popup__photo');
-      const photoFragment = document.createDocumentFragment();
-      ad.offer.photos.map((photo) => {
-        const photoElement = photoTemplate.cloneNode(true);
-        photoElement.src = photo;
-        photoFragment.appendChild(photoElement);
-      });
-      const photoBlock = adElement.querySelector('.popup__photos');
-      photoBlock.innerHTML = '';
-      photoBlock.appendChild(photoFragment);
-    }
-    else {
-      adElement.querySelector('.popup__photos').classList.add('hidden');
-    }
-    adsFragment.appendChild(adElement);
-  });
+  const adElement = adsTemplate.cloneNode(true);
+  ad.author.avatar ? adElement.querySelector('.popup__avatar').src = ad.author.avatar : adElement.querySelector('.popup__avatar').classList.add('hidden');
+  adElement.querySelector('.popup__title').textContent = ad.offer.title || '';
+  adElement.querySelector('.popup__text--address').textContent = ad.offer.address || '';
+  adElement.querySelector('.popup__text--price').innerHTML = ad.offer.price ? `${ad.offer.price}<span>₽/ночь</span>` : '';
+  adElement.querySelector('.popup__type').textContent = HOUSING_TYPE[ad.offer.type].name || '';
+  adElement.querySelector('.popup__text--capacity').textContent = (ad.offer.guests && ad.offer.rooms) ? `${ad.offer.rooms} ${getDeclension(ad.offer.rooms, ['комната', 'комнаты', 'комнат'])} для ${ad.offer.guests} ${getDeclension(ad.offer.guests, ['гостя', 'гостей', 'гостей'])}` : '';
+  adElement.querySelector('.popup__text--time').textContent = (ad.offer.checkin && ad.offer.checkout) ? `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}` : '';
+  adElement.querySelector('.popup__description').textContent = ad.offer.description || '';
+  if (ad.offer.features) {
+    const modifiers = ad.offer.features.map((feature) => `popup__feature--${feature}`);
+    const adFeaturesElements = adElement.querySelectorAll('.popup__feature');
+    adFeaturesElements.forEach((item) => {
+      const modifier = item.classList[1];
+      if (!modifiers.includes(modifier)) {
+        item.classList.add('hidden');
+      }
+    });
+  }
+  else {
+    adElement.querySelector('.popup__features').classList.add('hidden');
+  }
+  if (ad.offer.photos) {
+    const photoTemplate = adElement.querySelector('.popup__photo');
+    const photoFragment = document.createDocumentFragment();
+    ad.offer.photos.map((photo) => {
+      const photoElement = photoTemplate.cloneNode(true);
+      photoElement.src = photo;
+      photoFragment.appendChild(photoElement);
+    });
+    const photoBlock = adElement.querySelector('.popup__photos');
+    photoBlock.innerHTML = '';
+    photoBlock.appendChild(photoFragment);
+  }
+  else {
+    adElement.querySelector('.popup__photos').classList.add('hidden');
+  }
+  adsFragment.appendChild(adElement);
   return adsFragment;
 };
 
-export {generateAds};
+export {generateAd};
--- js/main.js
+++ js/main.js
@@ -1,50 +1,5 @@
-import {generateAds} from'./generate-ads.js';
-import {adForm, adSet} from './data.js';
 import {togglePageActiveState} from './page-state.js';
 import './validation.js';
+import './get-map.js';
 
-const mapCanvas = document.querySelector('#map-canvas');
-const allAdsFragment = generateAds(adSet);
 togglePageActiveState(true);
-
-const mainPinIcon = L.icon({
-  iconUrl: 'img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
-
-const map = L.map('map-canvas')
-  .on('load', () => {
-    togglePageActiveState(false);
-  })
-  .setView({
-    lat: 35.6804,
-    lng: 139.769,
-  }, 14);
-
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(map);
-
-const marker = L.marker(
-  {
-    lat: 35.6804,
-    lng: 139.769,
-  },
-  {
-    draggable: true,
-    icon: mainPinIcon,
-  },
-);
-
-marker.addTo(map);
-adForm['address'].value = `${marker.getLatLng().lat.toFixed(5)  }, ${  marker.getLatLng().lng.toFixed(5)}`;
-
-marker.on('moveend', (evt) => {
-  adForm['address'].value = `${evt.target.getLatLng().lat.toFixed(5)  }, ${  evt.target.getLatLng().lng.toFixed(5)}`;
-});
-
-
